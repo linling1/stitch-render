73ca836416b56664dd81b42ee18765c13c4db794
linling1 <470888961@qq.com>
2023-09-23 10:41:58 +0000

    u

--- supervisor.js
+++ supervisor.js
@@ -1,11 +1,22 @@
-const {fork} = require('child_process');
+const {fork, execSync} = require('child_process');
 const util = require("./lib/util")
 
 
+function killChildProcess() {
+    console.log("===== Kill Server =====");
+    let command = `ps aux | grep "node ./server.js" | awk '{print $2}' | xargs -I{} kill -9 {}`;
+    console.log(command);
+    try {
+        execSync(command);
+    } catch {
+        return;
+    }
+}
 
 
-function start() {
+function start(first) {
     util.killChrome();
+    console.log("########## START SERVER ##########")
     let ChildProcess = fork('./server.js', [], {env: {ENBALE_PROXY_POOL:'true'}});
     // let ChildProcess = fork('./server.js');
     ChildProcess.on('exit', function(code) {
@@ -13,10 +24,16 @@ function start() {
         if (code !== 0) {
             setTimeout(start, 1000);
         }
+        console.log("cycle restart server")
+        setTimeout(()=> killChildProcess(), 600000);
+        
     });
 
-    setTimeout(()=> ChildProcess.kill("SIGKILL"), 600000)
+    if(first) {
+        console.log("first to cycle restart server")
+        setTimeout(()=> killChildProcess(), 600000);
+    }
+    
 }
 
-start();
-
+start(true);
