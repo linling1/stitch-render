3dcc234dfb940ddc45c8e908c7f64146dbba6c7d
linling <470888961@qq.com>
2023-12-16 23:18:42 +0800

    catch global err and redefine console log (#15)

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
+const util = require("./lib/util")
+
+util.redefine_log()
+
+process.on('uncaughtException', function(err) {
+    console.error('Caught exception: ' + err.stack);
+});
 
 var server = prerender();
 
