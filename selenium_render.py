6364f7a85a3eeb3395385ecc22f27b10abbbce31
linling1 <470888961@qq.com>
2024-02-28 11:46:38 +0800

    u

--- selenium_render.py
+++ selenium_render.py
@@ -23,72 +23,81 @@ class SeleniumRender :
     
     
     def _get_driver(self, headless=True, mobile=False, proxy_host=None, run_js:bool=False, user_agent:str=None, loading_page_timeout:int=EXECUTOR_TIMEOUT, disable_proxy:bool=False, width:int=1440, height:int=718, chrome_path:str=None, version:str=None) -> webdriver.Chrome :
-        chrome_options = webdriver.ChromeOptions()
-        if headless:
-            chrome_options.add_argument("--headless=new")
+        try :
+            chrome_options = webdriver.ChromeOptions()
+            if headless:
+                chrome_options.add_argument("--headless=new")
 
-        if mobile:
-            # use to load as mobile
-            mobile_emulation = { "deviceName": "iPhone XR" }
-            chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-        else:
-            chrome_options.add_argument(
-                f'--user-agent={user_agent}'
-            )
+            if mobile:
+                # use to load as mobile
+                mobile_emulation = { "deviceName": "iPhone XR" }
+                chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+            else:
+                chrome_options.add_argument(
+                    f'--user-agent={user_agent}'
+                )
 
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation','enable-logging'])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # 就是这一行告诉chrome去掉了webdriver痕迹，令navigator.webdriver=false，极其关键
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--disable-notifications")
-        chrome_options.add_argument("--incognito")
-        chrome_options.add_argument('--ignore-certificate-errors')
-        # chrome_options.add_argument('--disable-gpu')
-        # chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument('--log-level=3')
-        chrome_options.add_argument(f'--window-size={width},{height}')
-        chrome_options.add_argument("--disable-popup-blocking")
-        chrome_options.add_argument("--profile-directory=Default")
-        chrome_options.add_argument("--disable-plugins-discovery")
-        chrome_options.add_argument('--lang=en-US')
-        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
-        capabilities = dict(DesiredCapabilities.CHROME)
-        if proxy_host is not None and not disable_proxy:
-            capabilities['proxy'] = {
-                'proxyType': 'MANUAL',
-                'httpProxy': proxy_host,
-                'sslProxy': proxy_host,
-                'noProxy': '',
-                'class': "org.openqa.selenium.Proxy",
-                'autodetect': False
-            }
-        capabilities['goog:loggingPrefs'] = { 'browser':'ALL' }
+            chrome_options.add_experimental_option('excludeSwitches', ['enable-automation','enable-logging'])
+            chrome_options.add_experimental_option('useAutomationExtension', False)
+            chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # 就是这一行告诉chrome去掉了webdriver痕迹，令navigator.webdriver=false，极其关键
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--start-maximized")
+            chrome_options.add_argument("--disable-notifications")
+            chrome_options.add_argument("--incognito")
+            chrome_options.add_argument('--ignore-certificate-errors')
+            # chrome_options.add_argument('--disable-gpu')
+            # chrome_options.add_argument("--disable-extensions")
+            chrome_options.add_argument('--log-level=3')
+            chrome_options.add_argument(f'--window-size={width},{height}')
+            chrome_options.add_argument("--disable-popup-blocking")
+            chrome_options.add_argument("--profile-directory=Default")
+            chrome_options.add_argument("--disable-plugins-discovery")
+            chrome_options.add_argument('--lang=en-US')
+            chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
+            capabilities = dict(DesiredCapabilities.CHROME)
+            if proxy_host is not None and not disable_proxy:
+                capabilities['proxy'] = {
+                    'proxyType': 'MANUAL',
+                    'httpProxy': proxy_host,
+                    'sslProxy': proxy_host,
+                    'noProxy': '',
+                    'class': "org.openqa.selenium.Proxy",
+                    'autodetect': False
+                }
+            capabilities['goog:loggingPrefs'] = { 'browser':'ALL' }
 
-        if chrome_path :
-            chrome_options.binary_location = chrome_path
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager(version=version).install()),
-                                        options=chrome_options, desired_capabilities=capabilities)
-        else :
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager(version=version).install()),
-                                        options=chrome_options, desired_capabilities=capabilities)
-        
-        if run_js :
-            js = open(os.path.join(os.path.dirname(__file__), './js/stealth.min.js')).read()
-            driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-                "source": js
-            })
+            if chrome_path :
+                chrome_options.binary_location = chrome_path
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager(version=version).install()),
+                                            options=chrome_options, desired_capabilities=capabilities)
+            else :
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager(version=version).install()),
+                                            options=chrome_options, desired_capabilities=capabilities)
             
-        driver.set_page_load_timeout(loading_page_timeout)
-            
-        return driver
+            if run_js :
+                js = open(os.path.join(os.path.dirname(__file__), './js/stealth.min.js')).read()
+                driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+                    "source": js
+                })
+                
+            driver.set_page_load_timeout(loading_page_timeout)
+                
+            return driver
+        except :
+            try :
+                driver.quit()
+            except :
+                pass
     
     
     def close(self) -> None :
         print("========= close =========")
         if self.driver :
-            self.driver.quit()
+            try :
+                self.driver.quit()
+            except :
+                pass
 
     
     def __enter__(self):
