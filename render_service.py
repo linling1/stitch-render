71971cf18a33501511bf38e8c3ee81aa8c4398e2
linling <470888961@qq.com>
2024-06-29 11:50:25 +0800

    u (#28)

--- render_service.py
+++ render_service.py
@@ -20,10 +20,11 @@ output_html = """
 
 class RenderService:
     
-    def __init__(self, chrome_path:str=None) -> None:
+    def __init__(self, chrome_path:str) -> None:
         self.chrome_path = chrome_path
     
-    def render(url:str, render_type:str="json", user_agent:str=None, headers:dict=None, cookies:dict=None, proxy_url:str=None, loading_page_timeout:int=EXECUTOR_TIMEOUT, refresh:bool=False, javascript:str=None, disable_proxy:bool=False, delay:float=None, width:int=1440, height:int=718, full_page:bool=False, disable_pop:bool=True, incognito:bool=True) -> str :
+    
+    def render(self, url:str, render_type:str="json", user_agent:str=None, headers:dict=None, cookies:dict=None, proxy_url:str=None, loading_page_timeout:int=EXECUTOR_TIMEOUT, refresh:bool=False, javascript:str=None, disable_proxy:bool=False, delay:float=None, width:int=1440, height:int=718, full_page:bool=False, disable_pop:bool=True, incognito:bool=True) -> str :
         try :
             proxy_host = proxy_url if proxy_url else get_proxy()
             user_agent = user_agent if user_agent else USER_AGENT_POOL[random.randint(0, len(USER_AGENT_POOL) - 1)]
