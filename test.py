d37ef8545c3fecc81beb2c315bcca5994e6d94d5
linling <470888961@qq.com>
2024-08-20 10:26:13 +0800

    add actions logic (#31)

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 
-
 from drission_page_render import DrissionPageRender
 import time
 import logging
 import sys
+import json
 
 logging.basicConfig(
     stream=sys.stderr,
@@ -16,10 +16,7 @@ user_agent = "Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) 
 with DrissionPageRender(headless=False, user_agent=user_agent, chrome_path=chrome_path, loading_page_timeout=30, proxy_host="http://172.28.16.140:3128", disable_proxy=True) as page :
     url = "https://www.meganslaw.ca.gov/?searchType=City"
     # page.set.load_mode.none()  # 设置加载模式为none
-    cookies = {".AspNetCore.Antiforgery.VyLW6ORzMgk": "CfDJ8P4MjYHqlLhAr34x3NlJZW5S_yEgzbSZI6pCaKg-_vT_NeEQE1CMfhgPU_mp5rSnuI15of2nl-ckyPcLNjwWlCyuUQ8ittteeIsI2rKmx_RCGZmQlPBVIQfHxvg2SZhbppEBRuhKeVC6zJX927Ks1k8",
-               ".AspNetCore.Mvc.CookieTempDataProvider": "CfDJ8P4MjYHqlLhAr34x3NlJZW74Mqp-cVYmTB2T_toiiKzJSlNsE7OcXATBRIcrq2i8wIZ4C8AihXD4_eAEpGFbv8h0CPVo1u9pcW7H1Ko_K9bcwv9zKItMelcrI2G_Tmi8GkFJ5-X_hoSaBnDj6K3MnREt72Tvhu6hTx0oxlpuFteGcchCW5-g9ONxgQg-FzWjxWkMlGSTT_9trfU2Hh4XtuM",
-               ".AspNetCore.Session": "CfDJ8P4MjYHqlLhAr34x3NlJZW7rsDT56W0YdizCgCKvTZRGRLNUT7qgugOhyVnfJXuLYh%2BQ1ASuLy7jMmgxtK3cvFprHjWtmt1n1L9s0IgkmAhrCuIvnJeo80BeFw%2BzHMrNId7PE2qv5HD%2BuHfk%2BK6qAMH%2F7kx9Jb63bT7LSO72Te55",
-               "mlpersist": "43443715.1.813038960.1355738112"}
+    cookies = {".AspNetCore.Antiforgery.VyLW6ORzMgk": "CfDJ8P4MjYHqlLhAr34x3NlJZW7aCb-cxkGC_r6xBlPeJQNJA4Q0IPMuTRLI5hR3vmZdIkdF19btRVhRulVJt09yNS5OjfCsXz36VNL9I_urnC5QWjY4v-DtKubkqpClxroeMJLD2zXWaI2MpSePBlResaE", ".AspNetCore.Mvc.CookieTempDataProvider": "CfDJ8P4MjYHqlLhAr34x3NlJZW6wyCInNau9-DVtmeNXDQPx5TUkypPzF1QdBOSJurahhprqEX6VSuzaV5b5r-HGz6_1QDeUYid4IwtZCKJXPx-aujVy5QZPF8tQAjWTwysMfGlgh3O3rj-IQigYGQYSWZdCYoK9wrsiUN3o6YmkGrKuXugJ1AenzdJ9FOSfbxVdU5VRffutUExfR8WC5GnqwEQ", ".AspNetCore.Session": "CfDJ8P4MjYHqlLhAr34x3NlJZW5np49r54RvrAVEbdo7k%2FDgZJY4b89V9X8nGH2iw0ok6L0SkpBOYsM1rmyHve0wXJKt0xWcYjGd3kCG8x7UKy5U9dHd3CILkKd1jre%2FAZOwbe09zYf710sJ46V5KOl6nkv%2FEbkypEU3cEPW9Y%2Ffd9Rd", "mlpersist": "1017001268.1.813038960.1361820672"}
     if cookies :
         cookie_param = []
         for k, v in cookies.items() :
@@ -52,13 +49,32 @@ with DrissionPageRender(headless=False, user_agent=user_agent, chrome_path=chrom
     # wf = open("/Users/linling/Desktop/a.png","wb")
     # wf.write(img_bytes)
     # wf.close()
+    # javascript = "document.getElementById('city-only-search').value = 'LOS ANGELES';document.getElementById('submitButton').click();var c = function () {document.getElementById('continueButton').click()};setInterval('c()',8000)"
     javascript = ""
     if javascript :
         js_ret = page.run_cdp("Runtime.evaluate", **{
             "expression": javascript
         })
         js_ret = js_ret.get('result',{}).get('value')
-        time.sleep(1)
+    
+    actions = [
+        "{\"type\":\"javascript\",\"command\":\"document.getElementById('city-only-search').value = 'LOS ANGELES';document.getElementById('submitButton').click();\"}",
+        "{\"type\":\"sleep\",\"command\":5}",
+        "{\"type\":\"javascript\",\"command\":\"document.getElementById('continueButton').click()\"}",
+        "{\"type\":\"sleep\",\"command\":5}",
+        "{\"type\":\"javascript\",\"command\":\"document.getElementById('continueButton').click()\"}",
+    ]
+    if actions :
+        for action in actions :
+            action_kv = json.loads(action)
+            k = action_kv.get('type')
+            command = action_kv.get('command')
+            if k == 'javascript' :
+                js_ret = page.run_cdp("Runtime.evaluate", **{
+                    "expression": command
+                })
+            elif k == 'sleep' :
+                time.sleep(command)
     time.sleep(10)
     print(page.html, file=open('/Users/linling/Desktop/a.html', 'w'))  # 打印数据包正文
     
